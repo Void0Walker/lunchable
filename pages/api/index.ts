f8ad849c251123ad8931d1171c244af4c461561a
Void0Walker <arturas@kalandarisvili.com>
2022-10-07 00:10:46 +0200

    encoding issues

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -1,33 +1,44 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 import * as htmlparser2 from "htmlparser2";
-import axios from "axios";
-import { selectAll } from "css-select";
+import axios, { responseEncoding } from "axios";
+import { selectAll, selectOne } from "css-select";
+import iconv from "iconv-lite";
+import * as windows1250 from "windows-1250";
 
-type Data = {
-  name: string;
+type ServerError = {
+  error: string;
 };
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+export default async function handler(_req: NextApiRequest, res: NextApiResponse) {
   if (!process.env.MENU_PROVIDER) {
-    res.status(500).json({ name: "No menu provider set" });
+    res.status(500).json({ error: "No menu provider set" });
     return;
   }
 
-  const response = await axios.get(process.env.MENU_PROVIDER);
+  try {
+    const response = await axios.get(process.env.MENU_PROVIDER, { responseType: "arraybuffer" });
 
-  if (response.status !== 200 || !response.data) {
-    res.status(500).json({ name: "Failed to fetch menu" });
-    return;
+    const buffer = Buffer.from(response.data).toString("utf8");
+
+    const dom = htmlparser2.parseDocument(buffer);
+
+    const menu = selectAll(".menicka_detail", dom).map((item) => {
+      const name = selectOne(".nazev", item)?.children[0];
+
+      if (name && "children" in name) {
+        if ("data" in name.children[0]) {
+          return name.children[0].data;
+        }
+
+        return "N/A";
+      }
+
+      return "name";
+    });
+
+    res.setHeader("content-type", "application/json;charset=utf-8").status(200).json({ menu });
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch menu" });
   }
-
-  const dom = htmlparser2.parseDocument(response.data);
-
-  const menu = selectAll(".menicka_detail .hlavicka .info .nazev", dom).map((item) => {
-    return item.childNodes[0];
-  });
-
-  console.log(menu);
-
-  res.status(200).json({ name: "John Doe" });
 }
